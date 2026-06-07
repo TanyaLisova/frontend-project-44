0602d85bd2320a9714d7355bc89f01a38fcd6e71
Tanya Lisova <kitsunelia@gmail.com>
2026-06-07 11:27:29 +0300

    add brain-even game

---
+++ bin/brain-even.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import { askName }  from "../src/cli.js"
+import readlineSync from  'readline-sync';
+
+let count = 0
+// let rounds = 3
+
+const generateNum = () => {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+const even = (n) => n % 2 === 0;
+
+const answer = () => {
+    return readlineSync.question('Your answer: ');
+}
+
+const game = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = askName()
+    console.log(`Hello, ${name}`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    while (count < 3) {
+        let num = generateNum() 
+        console.log(`Question: ${num}`);
+
+        let correct = even(num) ? 'yes' : 'no'
+
+        let answ = answer()
+
+        if (answ === correct) {
+            console.log('Correct!');
+            count++
+        } else {
+            console.log(`'${answ}' is wrong answer ;(. Correct answer was '${correct}'.`);
+            console.log(`Let's try again, ${name}!`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${name}`);
+    
+}
+
+game()
